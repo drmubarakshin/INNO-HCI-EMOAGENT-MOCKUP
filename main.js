146464113629c9b08e99573109a4108f0ef6d347
Damir Mubarakshin <DRMubarakshin@sberbank.ru>
2020-04-10 17:23:12 +0300

    Add messages manipulation

---
+++ main.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+    let questionNum = 1;
+
+    let firstAnswer = 'This is the first answer';
+    let secondAnswer = 'This is the second one';
+    let thirdAnswer = 'This is the third one';
+    let currAnswer = firstAnswer;
+    $('#chat-input').on('keypress', function(e) {
+        let newMessage = '';
+
+        if (e.which === 13) {
+            newMessage = $(this).val();
+            $(this).val('')
+            $('#chat').append('<div class="message message-question">' + newMessage + '</div>');
+            setTimeout(function() {
+                $('#chat').append('<div class="message message-answer">' + currAnswer + '</div>');
+                if (questionNum === 1)
+                    currAnswer = secondAnswer;
+                else if (questionNum === 2) {
+                    currAnswer = thirdAnswer;
+                }
+                questionNum++;
+            }, 5000);
+        }
+    });
+})
